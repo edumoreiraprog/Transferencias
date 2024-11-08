dfa63c6b2c611410d76ba8e1896d55d1722a3ba6
Eduardo Moreira <edumoreira462@gmail.com>
2024-11-08 10:39:09 -0300

    Se agrego a clase Menu

---
+++ src/Menu.java
@@ -0,0 +1,38 @@
+public class Menu {
+
+    public void menuPrincipal() {
+        System.out.println("===== Menú Principal =====");
+        System.out.println("1. Gestión de Titulares");
+        System.out.println("2. Gestión de Transferencias");
+        System.out.println("3. Generar archivo .txt para transferencias");
+        System.out.println("4. Salir");
+        System.out.println("==========================");
+        System.out.println("Seleccione una opción: ");
+    }
+
+    public void menuTitulares() {
+        System.out.println("== Gestión de Titulares ==");
+        System.out.println("1. Agregar Titular");
+        System.out.println("2. Consultar Titular por ID");
+        System.out.println("3. Actualizar Titular");
+        System.out.println("4. Eliminar Titular");
+        System.out.println("5. Listar todos los Titulares");
+        System.out.println("6. Volver al Menú Principal");
+        System.out.println("==========================");
+        System.out.println("Seleccione una opción: ");
+    }
+
+    /*
+    public void menuTransferencias() {
+        System.out.println("== Gestión de Transferencias ==");
+        System.out.println("1. Agregar Transferencia");
+        System.out.println("2. Consultar Transferencia por ID");
+        System.out.println("3. Actualizar Titular");
+        System.out.println("4. Eliminar Titular");
+        System.out.println("5. Listar todos los Titulares");
+        System.out.println("6. Volver al Menú Principal");
+        System.out.println("==========================");
+        System.out.println("Seleccione una opción: ");
+    }
+     */
+}
